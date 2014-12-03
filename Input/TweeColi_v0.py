9d5c273ae156ae3f599cf6e873ba0ea02da00f4f
Ryan Silva <ryan.jay.silva@gmail.com>
2014-12-03 19:32:01 +0000

    edited input node for Ara

--- Input/TweeColi_v0.py
+++ Input/TweeColi_v0.py
@@ -10,10 +10,10 @@ hi = 8
 lo = 6
 
 # Keys for @ryanjaysilva
-apiKey = 'fu4svJdREnATr3tNsLEhOvv5b'
-apiSecret = 'uKOhBYw5W3Rlk1RUtPtjo8DERQko8uiQi9l9VddWAU5pAzUZ6e'
-accessToken = '606389094-6HaxmuJZ6cqr4WzuYrBd5uPPWzEqnHar7X184jcv'
-accessTokenSecret = 'GkNQsYZI2L7Zr4prqlgFbgljDHKH6BtpPrOUwuIh9XPY7'
+#apiKey = 'fu4svJdREnATr3tNsLEhOvv5b'
+#apiSecret = 'uKOhBYw5W3Rlk1RUtPtjo8DERQko8uiQi9l9VddWAU5pAzUZ6e'
+#accessToken = '606389094-6HaxmuJZ6cqr4WzuYrBd5uPPWzEqnHar7X184jcv'
+#accessTokenSecret = 'GkNQsYZI2L7Zr4prqlgFbgljDHKH6BtpPrOUwuIh9XPY7'
 
 
 # Keys for @BUBacteria
@@ -23,10 +23,10 @@ accessTokenSecret = 'GkNQsYZI2L7Zr4prqlgFbgljDHKH6BtpPrOUwuIh9XPY7'
 #accessTokenSecret = 'Op5kNa6MCOTNtSHsoLoXZ4lT5He5JwMvBQOrdJpEqH4je'
 
 # Keys for @TweeColi
-#apiKey = 'RJSlPyCo084MJKX63tdygPNqZ'
-#apiSecret = 'E5i83RLt9VisK4zCngIyHBYxIJx46GD89oowaAiSq4BZeqLXiT'
-#accessToken = '2798012371-BYjTww8SShM4lUoh2RNpoAB4TJ1aZLcCQRGZUBc'
-#accessTokenSecret = 'neGup8dLJbdlezQb2FrEKNHsxYdQUJCZbDxJ10Iinch7x'
+apiKey = 'RJSlPyCo084MJKX63tdygPNqZ'
+apiSecret = 'E5i83RLt9VisK4zCngIyHBYxIJx46GD89oowaAiSq4BZeqLXiT'
+accessToken = '2798012371-BYjTww8SShM4lUoh2RNpoAB4TJ1aZLcCQRGZUBc'
+accessTokenSecret = 'neGup8dLJbdlezQb2FrEKNHsxYdQUJCZbDxJ10Iinch7x'
 
 # Set up twython
 api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret) 
@@ -49,12 +49,12 @@ while True:
 	# Test for movement from out of steady-state into steady state
   	if(not(hi > pHPre > lo) and (hi > pH > lo)):
 		#tweetStr = "Everything is ok! My pH is now %r" % pH
-		tweetStr = "aTc True at %s" % stamp
+		tweetStr = "Ara True at %s" % stamp
 		api.update_status(status=tweetStr)
 		print "Tweeted: " + tweetStr
 	# Test for movement from steady-state to outside of threshold
 	elif((hi > pHPre > lo) and not(hi > pH > lo)):
-		tweetStr = "aTc False at %s" % stamp
+		tweetStr = "Ara False at %s" % stamp
 		api.update_status(status=tweetStr)
 		print "Tweeted: " + tweetStr
 	# Print debugging message
